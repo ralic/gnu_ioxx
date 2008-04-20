0583b7f4e24d79384c39195b394112ac4a42c952
Peter Simons <simons@cryp.to>
2008-04-20 20:21:12 +0200

    Implemented a pretty neat task scheduler.

--- test/timeout.cpp
+++ test/timeout.cpp
@@ -1,103 +1,155 @@
+#include <ioxx/error.hpp>
+
 #include <boost/function.hpp>
 #include <boost/noncopyable.hpp>
-#include <boost/strong_typedef.hpp>
 #include <boost/compatibility/cpp_c_headers/ctime>
-#include <boost/system/system_error.hpp>
+#include <sys/time.h>
 #include <map>
+#include <boost/strong_typedef.hpp>
 
 namespace ioxx
 {
-  BOOST_STRONG_TYPEDEF(std::time_t, time_t);
-  BOOST_STRONG_TYPEDEF(unsigned int, second_t);
-  BOOST_STRONG_TYPEDEF(unsigned int, nanosecond_t);
+  using std::time_t;
+  typedef unsigned int seconds_t;
 
   class system_time : private boost::noncopyable
   {
   public:
     system_time()                       { update(); }
 
-    static time_t const &  now()        { return _now; }
-    static void            update();
+    time_t const &  as_time_t() const  { return _now.tv_sec; }
+    timeval const & as_timeval() const { return _now; }
+
+    void update()
+    {
+      throw_errno_if_minus1_<int>( "system call gettimeofday(2) failed"
+                                 , boost::bind(gettimeofday, &_now, static_cast<struct timezone *>(0))
+                                 );
+    }
 
   private:
-    static time_t _now;
+    timeval _now;
   };
 
-  time_t system_time::_now = ioxx::time_t(0);
-
-  void ioxx::system_time::update()
-  {
-    using namespace boost::system;
-    std::time_t new_now;
-    if (time(&new_now) == std::time_t(-1))
-      throw system_error(errno, errno_ecat, "cannot determine system time");
-    else
-      _now = new_now;
-  }
-
-  class timeout : public system_time
+  class scheduler : boost::noncopyable
   {
   public:
-    typedef boost::function<void ()>      handler;
-    typedef std::multimap<time_t,handler> handler_map;
-    typedef handler_map::iterator         iterator;
-    typedef handler_map::value_type       context;
-    typedef std::pair<time_t,iterator>    event;
+    typedef boost::function<void ()>            task;
+    typedef std::multimap<time_t,task>          task_queue;
+    typedef task_queue::iterator                queue_iterator;
+    typedef task_queue::value_type              queue_entry;
+    typedef std::pair<time_t,queue_iterator>    task_id;
 
-    event at(time_t const & to, handler const & f)
+    task_id at(time_t to, task const & f)
     {
       BOOST_ASSERT(f);
-      BOOST_ASSERT(to >= now());
-      return event(to, _the_map.insert(context(to, f)));
+      return task_id(to, _queue.insert(queue_entry(to, f)));
     }
 
-    event in(second_t seconds, handler const & f)
+    bool cancel(task_id & tid)
     {
-      return at(time_t(now() + seconds), f);
-    }
-
-    void cancel(event const & to)
-    {
-      if (to.first >= now()) _the_map.erase(to.second);
-    }
-
-    bool empty()
-    {
-      return _the_map.empty();
-    }
-
-    size_t deliver(second_t * next_in_seconds = 0)
-    {
-      update();
-      size_t n( 0u );
-      while (!empty())
+      queue_iterator i( _queue.begin() );
+      if (tid.first > i->first)
       {
-        iterator i( _the_map.begin() );
-        if (i->first < now())
+        _queue.erase(tid.second);
+        tid.first = static_cast<time_t>(0);
+        return true;
+      }
+      while (i->first == tid.first)
+      {
+        if (i == tid.second)
         {
-          ++n;
-          i->second();
-          _the_map.erase(i);
+          _queue.erase(tid.second);
+          tid.first = static_cast<time_t>(0);
+          return true;
         }
         else
-        {
-          if (next_in_seconds)
-            *next_in_seconds = static_cast<second_t>(i->first - now()) + 1u;
-          break;
-        }
+          ++i;
       }
-      return n;
+      return false;
+    }
+
+    bool cancel(task_id & tid, time_t now)
+    {
+      if (tid.first > now)
+      {
+        BOOST_ASSERT(_queue.begin()->first <= tid.first);
+        _queue.erase(tid.second);
+        tid.first = static_cast<time_t>(0);
+        return true;
+      }
+      else
+        return cancel(tid);
+    }
+
+    bool empty() const
+    {
+      return _queue.empty();
+    }
+
+    seconds_t run(time_t now)
+    {
+      queue_iterator i;
+      for (task f; !empty(); f.clear())
+      {
+        i = _queue.begin();
+        if (i->first <= now)
+        {
+          f.swap(i->second);
+          try { _queue.erase(i); }
+          catch(...)
+          {
+            f.swap(i->second);
+            throw;
+          }
+          f();
+        }
+        else
+          return static_cast<seconds_t>(i->first - now);
+      }
+      return 0;
     }
 
   private:
-    handler_map _the_map;
+    task_queue _queue;
   };
 
 } // namespace ioxx
 
-#include <boost/test/minimal.hpp>
+#define BOOST_AUTO_TEST_MAIN
+#include <boost/test/auto_unit_test.hpp>
 
-int test_main(int, char**)
+BOOST_AUTO_TEST_CASE( basic_system_time_test )
 {
-  ioxx::timeout timer;
+  ioxx::system_time now;
+  BOOST_CHECK_EQUAL(now.as_time_t(), std::time(0));
+  BOOST_CHECK_EQUAL(now.as_time_t(), now.as_timeval().tv_sec);
+}
+
+static size_t dummy_was_called = 0u;
+void dummy_function() { ++dummy_was_called; }
+
+BOOST_AUTO_TEST_CASE( basic_scheduler_test )
+{
+  ioxx::system_time now;
+  ioxx::scheduler schedule;
+  BOOST_CHECK(schedule.empty());
+  BOOST_CHECK_EQUAL(schedule.run(now.as_time_t()), 0u);
+  BOOST_CHECK_EQUAL(dummy_was_called, 0u);
+  schedule.at(now.as_time_t(), dummy_function);
+  schedule.at(now.as_time_t() + 1u, dummy_function);
+  ioxx::scheduler::task_id tid( schedule.at(now.as_time_t() + 5u, dummy_function) );
+  schedule.at(now.as_time_t() + 2u, boost::bind(&ioxx::scheduler::cancel, &schedule, tid));
+  ioxx::seconds_t delay( schedule.run(now.as_time_t()) );
+  BOOST_CHECK_EQUAL(delay, 1u);
+  BOOST_CHECK_EQUAL(dummy_was_called, 1u);
+  sleep(delay); now.update();
+  delay = schedule.run(now.as_time_t());
+  BOOST_CHECK_EQUAL(dummy_was_called, 2u);
+  BOOST_CHECK_EQUAL(delay, 1u);
+  sleep(delay); now.update();
+  delay = schedule.run(now.as_time_t());
+  BOOST_CHECK_EQUAL(dummy_was_called, 2u);
+  BOOST_CHECK_EQUAL(delay, 0u);
+  BOOST_CHECK(schedule.empty());
 }
