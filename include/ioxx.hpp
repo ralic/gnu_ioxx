a8284c4afd2cd9743325cc85e1c503ea9c4e9b2c
Peter Simons <simons@cryp.to>
2008-04-21 19:34:20 +0200

    Include sub-headers.

--- include/ioxx.hpp
+++ include/ioxx.hpp
@@ -1,5 +1,8 @@
 #ifndef IOXX_HPP_INCLUDED_2008_04_14
 #define IOXX_HPP_INCLUDED_2008_04_14
 
+#include "ioxx/error.hpp"
+#include "ioxx/timer.hpp"
+#include "ioxx/scheduler.hpp"
 
 #endif // IOXX_HPP_INCLUDED_2008_04_14
