bab11a615b94eee186ca86135ba161542e95f6c3
Peter Simons <simons@cryp.to>
2008-04-25 16:19:50 +0200

    Re-implemented what used to be "probe implementations" as proper stand-alone de-multiplexer classes. poll() is still missing though.

---
+++ include/ioxx/demux/epoll.hpp
@@ -0,0 +1,121 @@
+#ifndef IOXX_DEMUX_EPOLL_HPP_INCLUDED_2008_04_20
+#define IOXX_DEMUX_EPOLL_HPP_INCLUDED_2008_04_20
+
+#include "ioxx/error.hpp"
+#include <boost/noncopyable.hpp>
+#include <algorithm>
+#include <limits>
+#include <sys/epoll.h>
+
+namespace ioxx { namespace demux
+{
+  typedef unsigned int seconds_t;
+
+  class epoll : private boost::noncopyable
+  {
+  public:
+    class socket : private boost::noncopyable
+    {
+    public:
+      typedef int id;
+
+      enum event_set
+        { no_events = 0
+        , readable  = EPOLLIN
+        , writable  = EPOLLOUT
+        , pridata   = EPOLLPRI
+        };
+
+      friend inline event_set & operator|= (event_set & lhs, event_set rhs) { return lhs = (event_set)((int)(lhs) | (int)(rhs)); }
+      friend inline event_set   operator|  (event_set   lhs, event_set rhs) { return lhs |= rhs; }
+      friend inline event_set & operator&= (event_set & lhs, event_set rhs) { return lhs = (event_set)((int)(lhs) & (int)(rhs)); }
+      friend inline event_set   operator&  (event_set   lhs, event_set rhs) { return lhs &= rhs; }
+
+      socket(epoll & demux, id sock, event_set ev = no_events) : _demux(demux), _sock(sock)
+      {
+        BOOST_ASSERT(_sock >= 0);
+        epoll_event e;
+        e.data.fd = _sock;
+        e.events  = ev;
+        throw_errno_if_minus1("add socket into epoll", boost::bind(&epoll_ctl, _demux._epoll_fd, EPOLL_CTL_ADD, _sock, &e));
+      }
+
+      ~socket()
+      {
+        epoll_event e;
+        e.data.fd = _sock;
+        e.events  = 0;
+        throw_errno_if_minus1("del socket from epoll", boost::bind(&epoll_ctl, _demux._epoll_fd, EPOLL_CTL_DEL, _sock, &e));
+      }
+
+      void request(event_set ev)
+      {
+        epoll_event e;
+        e.data.fd = _sock;
+        e.events  = ev;
+        throw_errno_if_minus1("modify socket in epoll", boost::bind(&epoll_ctl, _demux._epoll_fd, EPOLL_CTL_MOD, _sock, &e));
+      }
+
+    private:
+      epoll &   _demux;
+      id const  _sock;
+    };
+
+    static seconds_t max_timeout()
+    {
+      return static_cast<seconds_t>(std::numeric_limits<int>::max() / 1000);
+    }
+
+    epoll(unsigned int size_hint = 128u) : _n_events(0u), _current(0u)
+    {
+      size_hint = std::min(size_hint, static_cast<unsigned int>(std::numeric_limits<int>::max()));
+      _epoll_fd = throw_errno_if_minus1("create epoll socket", boost::bind(&epoll_create, static_cast<int>(size_hint)));
+    }
+
+    ~epoll()
+    {
+      throw_errno_if_minus1("close epoll socket", boost::bind(&::close, _epoll_fd));
+    }
+
+    bool empty() const { return _n_events == 0u; }
+
+    bool pop_event(socket::id & sock, socket::event_set & ev)
+    {
+      IOXX_TRACE_MSG("epoll::pop_event() has " << _n_events << " events to deliver");
+      if (!_n_events) return false;
+      sock = _events[_current].data.fd;
+      ev   = static_cast<socket::event_set>(_events[_current].events);
+      BOOST_ASSERT(ev != socket::no_events);
+      --_n_events; ++_current;
+      return true;
+   }
+
+    void wait(seconds_t timeout)
+    {
+      BOOST_ASSERT(timeout <= max_timeout());
+      BOOST_ASSERT(!_n_events);
+      int const rc( epoll_wait( _epoll_fd
+                              , _events, sizeof(_events) / sizeof(epoll_event)
+                              , static_cast<int>(timeout) * 1000
+                              ));
+      IOXX_TRACE_MSG("epoll::wait() returned " << rc);
+      if (rc < 0)
+      {
+        if (errno == EINTR) return;
+        boost::system::system_error err(errno, boost::system::errno_ecat, "epoll_wait(2)");
+        throw err;
+      }
+      _n_events = static_cast<size_t>(rc);
+      _current    = 0u;
+    }
+
+  private:
+    socket::id  _epoll_fd;
+    epoll_event _events[128];
+    size_t      _n_events;
+    size_t      _current;
+  };
+
+}} // namespace ioxx::demux
+
+#endif // IOXX_DEMUX_EPOLL_HPP_INCLUDED_2008_04_20
---
+++ include/ioxx/demux/select.hpp
@@ -0,0 +1,144 @@
+#ifndef IOXX_DEMUX_SELECT_HPP_INCLUDED_2008_04_20
+#define IOXX_DEMUX_SELECT_HPP_INCLUDED_2008_04_20
+
+#include "ioxx/error.hpp"
+#include <boost/noncopyable.hpp>
+#include <algorithm>
+#include <limits>
+#include <sys/select.h>
+
+namespace ioxx { namespace demux
+{
+  typedef unsigned int seconds_t;
+
+  class select : private boost::noncopyable
+  {
+  public:
+    class socket : private boost::noncopyable
+    {
+    public:
+      typedef int id;
+
+      enum event_set
+        { no_events = 0
+        , readable  = 1 << 0
+        , writable  = 1 << 1
+        , pridata   = 1 << 2
+        };
+
+      friend inline event_set & operator|= (event_set & lhs, event_set rhs) { return lhs = (event_set)((int)(lhs) | (int)(rhs)); }
+      friend inline event_set   operator|  (event_set   lhs, event_set rhs) { return lhs |= rhs; }
+      friend inline event_set & operator&= (event_set & lhs, event_set rhs) { return lhs = (event_set)((int)(lhs) & (int)(rhs)); }
+      friend inline event_set   operator&  (event_set   lhs, event_set rhs) { return lhs &= rhs; }
+
+      socket(select & demux, id sock, event_set ev = no_events) : _demux(demux), _sock(sock)
+      {
+        BOOST_ASSERT(_sock >= 0);
+        BOOST_ASSERT(_sock <= FD_SETSIZE);
+        request(ev);
+      }
+
+      ~socket()
+      {
+        request(no_events);
+      }
+
+      void request(event_set ev)
+      {
+        if (ev & readable) { FD_SET(_sock, &_demux._req_read_fds); }   else { FD_CLR(_sock, &_demux._req_read_fds); }
+        if (ev & writable) { FD_SET(_sock, &_demux._req_write_fds); }  else { FD_CLR(_sock, &_demux._req_write_fds); }
+        if (ev & pridata)  { FD_SET(_sock, &_demux._req_except_fds); } else { FD_CLR(_sock, &_demux._req_except_fds); }
+        if (ev != no_events)
+        {
+          _demux._max_fd = std::max(_demux._max_fd, _sock);
+        }
+        else if (_sock == _demux._max_fd)
+        {
+          while(_demux._max_fd >= 0)
+          {
+            if (  FD_ISSET(_demux._max_fd, &_demux._req_read_fds)
+               || FD_ISSET(_demux._max_fd, &_demux._req_write_fds)
+               || FD_ISSET(_demux._max_fd, &_demux._req_except_fds)
+               )
+              break;
+            else
+              --_demux._max_fd;
+          }
+          IOXX_TRACE_MSG("select: new _max_fd is " << _demux._max_fd);
+        }
+        else
+          BOOST_ASSERT(_sock < _demux._max_fd);
+      }
+
+    private:
+      select &  _demux;
+      id const  _sock;
+    };
+
+    static seconds_t max_timeout()
+    {
+      return static_cast<seconds_t>(std::numeric_limits<int>::max());
+    }
+
+    select(unsigned int /* size_hint */ = 0u) : _max_fd(-1), _current(0), _n_events(0u)
+    {
+      FD_ZERO(&_req_read_fds);
+      FD_ZERO(&_req_write_fds);
+      FD_ZERO(&_req_except_fds);
+    }
+
+    bool empty() const { return _n_events == 0u; }
+
+    bool pop_event(socket::id & sock, socket::event_set & ev)
+    {
+      while (_n_events)
+      {
+        IOXX_TRACE_MSG("select::pop_event() has " << _n_events << " events to deliver; _max_fd = " << _max_fd << "; _current = " << _current);
+        BOOST_ASSERT(_current <= _max_fd);
+        sock = _current++;
+        BOOST_ASSERT(sock >= 0);
+        ev = socket::no_events;
+        if (FD_ISSET(sock, &_recv_read_fds))   { --_n_events; ev |= socket::readable; }
+        if (FD_ISSET(sock, &_recv_write_fds))  { --_n_events; ev |= socket::writable; }
+        if (FD_ISSET(sock, &_recv_except_fds)) { --_n_events; ev |= socket::pridata; }
+        if (ev != socket::no_events) return true;
+      }
+      return false;
+    }
+
+    void wait(seconds_t timeout)
+    {
+      BOOST_ASSERT(timeout <= max_timeout());
+      BOOST_ASSERT(!_n_events);
+      timeval tv = { timeout, 0 };
+      if (_max_fd == -1)
+      {
+        ::select(0, NULL, NULL, NULL, &tv);
+        return;
+      }
+      _recv_read_fds   = _req_read_fds;
+      _recv_write_fds  = _req_write_fds;
+      _recv_except_fds = _req_except_fds;
+      int const rc( ::select(_max_fd + 1, &_recv_read_fds, &_recv_write_fds, &_recv_except_fds, &tv) );
+      IOXX_TRACE_MSG("select::wait() returned " << rc);
+      if (rc < 0)
+      {
+        if (errno == EINTR) return;
+        boost::system::system_error err(errno, boost::system::errno_ecat, "select(2)");
+        throw err;
+      }
+      _n_events  = static_cast<size_t>(rc);
+      _current = 0;
+    }
+
+  private:
+    fd_set      _req_read_fds, _req_write_fds, _req_except_fds;
+    fd_set      _recv_read_fds, _recv_write_fds, _recv_except_fds;
+    socket::id  _max_fd;
+    socket::id  _current;
+    size_t      _n_events;
+  };
+
+}} // namespace ioxx::demux
+
+#endif // IOXX_DEMUX_SELECT_HPP_INCLUDED_2008_04_20
