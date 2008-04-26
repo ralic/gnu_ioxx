81f1849f8087a7ae156f866a0f9d28467a75b511
Peter Simons <simons@cryp.to>
2008-04-26 20:49:42 +0200

    Added poll demuxer.

--- include/ioxx/demux.hpp
+++ include/ioxx/demux.hpp
@@ -5,12 +5,30 @@
 #  include "demux/epoll.hpp"
 namespace ioxx { typedef demux::epoll default_demux; }
 #elif defined(IOXX_HAVE_POLL)
-// todo
+#  include "demux/poll.hpp"
+namespace ioxx { typedef demux::poll<> default_demux; }
 #elif defined(IOXX_HAVE_SELECT)
 #  include "demux/select.hpp"
 namespace ioxx { typedef demux::select default_demux; }
 #else
 #  error "No I/O de-multiplexer available for this platform."
 #endif
+
+namespace ioxx { namespace demux
+{
+  template <class T>
+  struct demux_concept
+  {
+    void constraints()
+    {
+    }
+  };
+
+  class demux_archetype
+  {
+  public:
+  };
+
+}} // namespace ioxx::demux
 
 #endif // IOXX_DEMUX_HPP_INCLUDED_2008_04_20
