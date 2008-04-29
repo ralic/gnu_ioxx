c3d4add2f7754d3554be4e1c68b1e97836f2b6ab
Peter Simons <simons@cryp.to>
2008-04-29 23:43:18 +0200

    added documentation stubs

--- include/ioxx.hpp
+++ include/ioxx.hpp
@@ -1,8 +1,26 @@
 #ifndef IOXX_HPP_INCLUDED_2008_04_14
 #define IOXX_HPP_INCLUDED_2008_04_14
 
-#include "ioxx/error.hpp"
-#include "ioxx/timer.hpp"
-#include "ioxx/scheduler.hpp"
+/** \mainpage Asynchronous I/O for C++
+ *
+ * \section toc_sec Table of Contents
+ *
+ * \li \ref intro_sec
+ * \li \ref reference_sec "References"
+ *
+ * \section intro_sec Introduction
+ *
+ * When you write applications that have to read data from a socket, your code
+ * usually contains statements such as the following:
+ *
+ * \code
+ *    int sock = ... ;
+ *    char buf[1024];
+ *    ssize_t rc = read(sock, buf, sizeof(buf));
+ * \endcode
+ *
+ * \anchor reference_sec
+ * \see Boost Homepage: http://boost.org/
+ */
 
 #endif // IOXX_HPP_INCLUDED_2008_04_14
