55daf4fbc727d99fd9270d49cbee617bc3ff9e9b
Peter Simons <simons@cryp.to>
2008-05-02 21:11:18 +0200

    Added a brand-new acceptor class to create listener sockets.

---
+++ include/ioxx/acceptor.hpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2008 Peter Simons <simons@cryp.to>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Copying and distribution of this file, with or without modification, are
+ * permitted in any medium without royalty provided the copyright notice and
+ * this notice are preserved.
+ */
+
+#ifndef IOXX_ACCEPTOR_HPP_INCLUDED_2008_04_20
+#define IOXX_ACCEPTOR_HPP_INCLUDED_2008_04_20
+
+#include "dispatch.hpp"
+#include <boost/function/function2.hpp>
+
+namespace ioxx
+{
+  template < class Dispatch = dispatch<>
+           , class Handler  = boost::function2<void, native_socket_t, typename Dispatch::socket::address const &>
+           >
+  class acceptor : public Dispatch::socket
+  {
+  public:
+    typedef Dispatch                    dispatch;
+    typedef typename dispatch::socket   socket;
+    typedef typename socket::address    address;
+    typedef typename socket::endpoint   endpoint;
+    typedef typename socket::event_set  event_set;
+    typedef Handler                     handler;
+
+    acceptor(dispatch & disp, endpoint const & addr, handler const & f = handler())
+    : socket(disp, socket::create(addr), boost::bind(&acceptor::run, this), socket::readable)
+    , _f(f)
+    {
+      this->set_nonblocking();
+      this->reuse_bind_address();
+      this->bind(addr);
+      this->listen(16u);
+    }
+
+  private:
+    handler     _f;
+
+    void run()
+    {
+      native_socket_t s;
+      address addr;
+      for(;;)
+      {
+        addr.as_socklen_t() = sizeof(sockaddr);
+        s = detail::throw_errno_if( detail::not_ewould_block(), "accept(2)"
+                                  , boost::bind(&::accept, this->as_native_socket_t(), &addr.as_sockaddr(), &addr.as_socklen_t())
+                                  );
+        if (s < 0) break;
+        detail::socket new_socket(s); // act as scope guard
+        new_socket.set_nonblocking();
+        new_socket.set_linger_timeout(0);
+        _f(s, addr);
+        new_socket.close_on_destruction(false);
+      }
+    }
+  };
+
+} // namespace ioxx
+
+#endif // IOXX_ACCEPTOR_HPP_INCLUDED_2008_04_20
--- test/dispatch.cpp
+++ test/dispatch.cpp
@@ -13,72 +13,10 @@
 #include "ioxx/time.hpp"
 #include "ioxx/schedule.hpp"
 #include "ioxx/dispatch.hpp"
+#include "ioxx/acceptor.hpp"
 #include <boost/shared_ptr.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/array.hpp>
-#include <boost/function.hpp>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-
-namespace ioxx
-{
-  typedef boost::function<void (native_socket_t, detail::socket::address const &)> socket_handler;
-
-  inline void accept_socket(native_socket_t ls, socket_handler f)
-  {
-    using namespace ioxx::detail;
-    IOXX_TRACE_SOCKET(ls, "listen socket has become readable");
-    native_socket_t     s;
-    sockaddr            addr;
-    socklen_t           len;
-    for (;;)
-    {
-      len = sizeof(sockaddr);
-      s   = throw_errno_if(not_ewould_block(), "accept(2)", boost::bind(&::accept,ls, &addr, &len));
-      if (s < 0) break;
-      socket sock(s);           // acts as scope guard in case of exceptions
-      sock.set_nonblocking();
-      f(s, detail::socket::address(addr, len));
-      sock.close_on_destruction(false);
-    }
-    IOXX_TRACE_SOCKET(ls, "no more pending connections");
-  }
-
-  template <class Demuxer, class Handler, class Allocator>
-  inline
-  typename dispatch<Demuxer,Handler,Allocator>::socket *
-  accept_stream_socket( dispatch<Demuxer,Handler,Allocator> & p
-                      , char const * node, char const * service
-                      , socket_handler const & f
-                      )
-  {
-    using namespace detail;
-    typedef dispatch<Demuxer,Handler,Allocator> dispatch;
-    typedef typename dispatch::socket           dispatch_socket;
-    boost::shared_ptr<addrinfo> _addr;
-    addrinfo const hint = { AI_NUMERICHOST, 0, SOCK_STREAM, 0, 0u, NULL, NULL, NULL };
-    addrinfo * addr;
-    int const rc( getaddrinfo(node, service, &hint, &addr) );
-    if (rc != 0) throw std::runtime_error( gai_strerror(rc) );
-    _addr.reset(addr, &freeaddrinfo);
-    while (addr && addr->ai_socktype != SOCK_STREAM)
-      addr = addr->ai_next;
-    if (!addr) throw std::runtime_error("address does not map to a stream socket endpoint");
-    socket ls( throw_errno_if_minus1( "socket(2)"
-                                    , boost::bind(&::socket, addr->ai_family, addr->ai_socktype, addr->ai_protocol)
-                                    ));
-    throw_errno_if_minus1("bind(2)", boost::bind(&bind, ls.as_native_socket_t(), addr->ai_addr, addr->ai_addrlen));
-    throw_errno_if_minus1("listen(2)", boost::bind(&listen, ls.as_native_socket_t(), 16u));
-    ls.set_nonblocking();
-    dispatch_socket * ps( new dispatch_socket(p, ls.as_native_socket_t(), boost::bind(&accept_socket, ls.as_native_socket_t(), f), dispatch_socket::readable) );
-    ls.close_on_destruction(false);
-    return ps;
-  }
-}
-
-///// Tests ///////////////////////////////////////////////////////////////////
 
 #define BOOST_AUTO_TEST_MAIN
 #include <boost/test/included/unit_test.hpp>
@@ -157,28 +95,44 @@ public:
     f.reset( new echo(sched, now) );
     socket * sock( new socket(disp, s, boost::bind(&echo::run, f, _1), socket::readable) );
     f->_sock.reset(sock);
+    f->run(socket::no_events);
   }
 };
 
 BOOST_AUTO_TEST_CASE( test_echo_handler )
 {
-  ioxx::time        now;
-  ioxx::schedule<>  schedule;
-  ioxx::dispatch<>  dispatch;
-  boost::scoped_ptr<ioxx::dispatch<>::socket> ls;
-  ls.reset(ioxx::accept_stream_socket(dispatch, "127.0.0.1", "8080", boost::bind(&echo::accept, boost::ref(schedule), boost::ref(now.as_time_t()), boost::ref(dispatch), _1, _2)));
-  IOXX_TRACE_SOCKET(ls, "accepting connections on port 8080");
-  schedule.at(now.as_time_t() + 5, boost::bind(&boost::scoped_ptr<ioxx::dispatch<>::socket> ::reset, &ls, static_cast<ioxx::dispatch<>::socket *>(0)));
+  typedef ioxx::time                    time;
+  typedef ioxx::schedule<>              schedule;
+  typedef ioxx::dispatch<>              dispatch;
+  typedef dispatch::socket              socket;
+  typedef socket::address               address;
+  typedef socket::endpoint              endpoint;
+  typedef ioxx::acceptor<dispatch>      acceptor;
+  typedef boost::scoped_ptr<acceptor>   acceptor_ptr;
+
+  using boost::bind;
+  using boost::ref;
+
+  time          now;
+  schedule      sched;
+  dispatch      disp;
+  acceptor_ptr  ls;
+  ls.reset( new acceptor( disp, endpoint("127.0.0.1", "8080")
+                        , bind(&echo::accept, ref(sched), ref(now.as_time_t()), ref(disp), _1, _2)
+                        ));
+  IOXX_TRACE_SOCKET(*ls, "accepting connections on port 8080");
+  sched.at(now.as_time_t() + 5, bind(&acceptor_ptr::reset, ref(ls), static_cast<acceptor*>(0)));
   for (;;)
   {
-    dispatch.run();
-    ioxx::seconds_t timeout( schedule.run(now.as_time_t()) );
-    if (schedule.empty())
+    disp.run();
+    ioxx::seconds_t timeout( sched.run(now.as_time_t()) );
+    if (sched.empty())
     {
-      if (dispatch.empty())  break;
-      else                   timeout = dispatch.max_timeout();
+      if (disp.empty()) break;
+      else              timeout = disp.max_timeout();
     }
-    dispatch.wait(timeout);
+    disp.wait(timeout);
     now.update();
   }
+  IOXX_TRACE_MSG("shutting down");
 }
