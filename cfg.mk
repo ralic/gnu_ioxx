204a5d3aa3d3d74287a65cdd514b51e48b4e3efe
Peter Simons <simons@cryp.to>
2010-03-01 19:19:44 +0100

    post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -2,7 +2,7 @@
 
 gnu_rel_host    := dl.sv.nongnu.org
 upload_dest_dir_:= /releases/ioxx/
-old_NEWS_hash   := d41d8cd98f00b204e9800998ecf8427e
+old_NEWS_hash   := 34f3713a9bc8d6c450f18f611f3e78ad
 gpg_key_ID      := 99089D72
 url_dir_list    := http://download.savannah.nongnu.org/releases/ioxx
 today           := $(date "+%Y-%m-%d")
