f1099dc93f5c903b553d211d31e3775260f650d6
Peter Simons <simons@cryp.to>
2010-03-04 16:35:22 +0100

    Re-released the library under LGPLv3+.

--- Makefile
+++ Makefile
@@ -1,15 +1,22 @@
 # Build the scheduler test program.
+#
+# Copyright (c) 2001-2010 Peter Simons <simons@cryp.to>
+#
+# Copying and distribution of this file, with or without modification, are
+# permitted in any medium without royalty provided the copyright notice
+# and this notice are preserved. This file is offered as-is, without any
+# warranty.
 
 CXX		= g++
 CPPFLAGS	=
 CXXFLAGS	= -O3 -pipe -Wall
 LDFLAGS		=
 
-.cpp.o:
+.cc.o:
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $<
 
 test:		test.o
-	$(CXX) $(LDFLAGS) -o $@ test.o
+	$(CXX) $(LDFLAGS) -o $@ $<
 
 clean::
 	rm -f test test.o
--- pollvector.hh
+++ pollvector.hh
@@ -1,6 +1,18 @@
 /*
- * Copyright (c) 2001 by Peter Simons <simons@ieee.org>.
- * All rights reserved.
+ * Copyright (c) 2001-2010 Peter Simons <simons@cryp.to>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef POLLVECTOR_HH
