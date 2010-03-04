eb1f91a2775009e61b33097938ff04e2a9e800fe
Peter Simons <simons@cryp.to>
2010-03-04 22:53:22 +0100

    Replaced all tabs by white space.

--- pollvector.hh
+++ pollvector.hh
@@ -31,73 +31,73 @@ class pollvector
     {
   public:
     explicit pollvector() : array(0), array_size(0), array_len(0)
-	{
-	reserve(1);
-	}
+        {
+        reserve(1);
+        }
 
     ~pollvector()
-	{
-	if (array)
-	    free(array);
-	}
+        {
+        if (array)
+            free(array);
+        }
 
     pollfd& operator[](int fd)
-	{
-	reserve(array_len+1);
+        {
+        reserve(array_len+1);
         pollfd tmp;
         tmp.fd = fd;
-	std::pair<pollfd*,pollfd*> i = std::equal_range(array, array+array_len, tmp, pollfd_less());
-	if (i.first - i.second > 1)
-	    throw std::logic_error("scheduler::pollvector: The internal poll array is broken!");
-	else if (i.first == i.second)
-	    {			// We have to insert it.
-	    memmove(i.first+1, i.first, (array+array_len-i.first)*sizeof(pollfd));
-	    ++array_len;
-	    i.first->fd      = fd;
-	    i.first->events  = 0;
-	    i.first->revents = 0;
-	    return *i.first;
-	    }
-	else
-	    return *i.first;	// Found it.
-	}
+        std::pair<pollfd*,pollfd*> i = std::equal_range(array, array+array_len, tmp, pollfd_less());
+        if (i.first - i.second > 1)
+            throw std::logic_error("scheduler::pollvector: The internal poll array is broken!");
+        else if (i.first == i.second)
+            {                   // We have to insert it.
+            memmove(i.first+1, i.first, (array+array_len-i.first)*sizeof(pollfd));
+            ++array_len;
+            i.first->fd      = fd;
+            i.first->events  = 0;
+            i.first->revents = 0;
+            return *i.first;
+            }
+        else
+            return *i.first;    // Found it.
+        }
 
     void erase(int fd)
-	{
+        {
         pollfd tmp;
         tmp.fd = fd;
-	std::pair<pollfd*,pollfd*> i = std::equal_range(array, array+array_len, tmp, pollfd_less());
-	if (i.first - i.second > 1)
-	    throw std::logic_error("scheduler::pollvector: The interal poll array is broken!");
-	else if (i.first == i.second)
-	    return;
-	else
-	    {			// Found it.
-	    memmove(i.first, i.first+1, (array+array_len-i.first-1)*sizeof(pollfd));
-	    reserve(--array_len);
-	    }
-	}
+        std::pair<pollfd*,pollfd*> i = std::equal_range(array, array+array_len, tmp, pollfd_less());
+        if (i.first - i.second > 1)
+            throw std::logic_error("scheduler::pollvector: The interal poll array is broken!");
+        else if (i.first == i.second)
+            return;
+        else
+            {                   // Found it.
+            memmove(i.first, i.first+1, (array+array_len-i.first-1)*sizeof(pollfd));
+            reserve(--array_len);
+            }
+        }
 
     size_t length() const throw()
-	{
-	return array_len;
-	}
+        {
+        return array_len;
+        }
 
     pollfd* get_pollfd_array() throw()
-	{
-	return array;
-	}
+        {
+        return array;
+        }
 
 #if 0
     void dump(std::ostream& os) const
-	{
-	os << "The poll vector has " << array_len << " entries; size is " << array_size << "." << std::endl;
-	for (size_t i = 0; i < array_len; ++i)
-	    os << "fd = "        << std::dec << array[i].fd << "; "
-	       << "events = 0x"  << std::hex << array[i].events << "; "
-	       << "revents = 0x" << std::hex << array[i].revents
-	       << std::dec << std::endl;
-	}
+        {
+        os << "The poll vector has " << array_len << " entries; size is " << array_size << "." << std::endl;
+        for (size_t i = 0; i < array_len; ++i)
+            os << "fd = "        << std::dec << array[i].fd << "; "
+               << "events = 0x"  << std::hex << array[i].events << "; "
+               << "revents = 0x" << std::hex << array[i].revents
+               << std::dec << std::endl;
+        }
 #endif
 
   private:
@@ -105,36 +105,36 @@ class pollvector
     pollvector& operator=(const pollvector&);
 
     void reserve(size_t size)
-	{
-	size_t  new_size;
-	pollfd* new_array;
+        {
+        size_t  new_size;
+        pollfd* new_array;
 
-	if (size <= array_len)
-	    return;		// Bullshit.
+        if (size <= array_len)
+            return;             // Bullshit.
 #if 0
-	else if (size < array_size/2 && array_size/2 > MIN_SIZE)
-	    {			// Shrink array.
-	    for (size_t n = array_size/2; n > size && n > MIN_SIZE; n /= 2)
-		new_size = n;
-	    debug("Shrinking pollvec array from %d to %d slots; we have %d entries.", array_size, new_size, array_len);
-	    new_array = static_cast<pollfd*>(realloc(array, new_size*sizeof(pollfd)));
-	    if (new_array == 0)
-		return;		// How is this supposed to happen?
-	    array      = new_array;
-	    array_size = new_size;
-	    }
+        else if (size < array_size/2 && array_size/2 > MIN_SIZE)
+            {                   // Shrink array.
+            for (size_t n = array_size/2; n > size && n > MIN_SIZE; n /= 2)
+                new_size = n;
+            debug("Shrinking pollvec array from %d to %d slots; we have %d entries.", array_size, new_size, array_len);
+            new_array = static_cast<pollfd*>(realloc(array, new_size*sizeof(pollfd)));
+            if (new_array == 0)
+                return;         // How is this supposed to happen?
+            array      = new_array;
+            array_size = new_size;
+            }
 #endif
-	else if (size > array_size)
-	    {			// Enlarge array.
-	    for (new_size = (array_size > 0) ? array_size*2 : MIN_SIZE; new_size < size; new_size *= 2)
-		;
-	    new_array = static_cast<pollfd*>(realloc(array, new_size*sizeof(pollfd)));
-	    if (new_array == 0)
-		throw std::bad_alloc();
-	    array      = new_array;
-	    array_size = new_size;
-	    }
-	}
+        else if (size > array_size)
+            {                   // Enlarge array.
+            for (new_size = (array_size > 0) ? array_size*2 : MIN_SIZE; new_size < size; new_size *= 2)
+                ;
+            new_array = static_cast<pollfd*>(realloc(array, new_size*sizeof(pollfd)));
+            if (new_array == 0)
+                throw std::bad_alloc();
+            array      = new_array;
+            array_size = new_size;
+            }
+        }
 
     static const size_t MIN_SIZE = 32;
 
@@ -143,9 +143,9 @@ class pollvector
     size_t  array_len;
 
     struct pollfd_less
-	{
+        {
         bool operator()(const pollfd& lhs, const pollfd& rhs) { return lhs.fd < rhs.fd; }
-	};
+        };
     };
 
 #endif // !defined(POLLVECTOR_HH)
--- test.cc
+++ test.cc
@@ -25,49 +25,49 @@ class my_handler : public scheduler::event_handler
     {
   public:
     my_handler(scheduler& sched) : mysched(sched)
-	{
-	}
+        {
+        }
     ~my_handler()
-	{
-	}
+        {
+        }
     scheduler::handler_properties prop;
   private:
     virtual void fd_is_readable(int fd)
-	{
-	int rc = read(fd, &tmp, sizeof(tmp));
-	if (rc == 0)
-	    mysched.remove_handler(0);
-	else if (rc > 0)
-	    {
-	    buffer.append(tmp, rc);
-	    prop.poll_events = POLLOUT;
-	    mysched.register_handler(1, *this, prop);
-	    }
-	}
+        {
+        int rc = read(fd, &tmp, sizeof(tmp));
+        if (rc == 0)
+            mysched.remove_handler(0);
+        else if (rc > 0)
+            {
+            buffer.append(tmp, rc);
+            prop.poll_events = POLLOUT;
+            mysched.register_handler(1, *this, prop);
+            }
+        }
     virtual void fd_is_writable(int fd)
-	{
-	if (buffer.empty())
-	    mysched.remove_handler(1);
-	int rc = write(fd, buffer.data(), buffer.length());
-	if (rc > 0)
-	    buffer.erase(0, rc);
-	}
+        {
+        if (buffer.empty())
+            mysched.remove_handler(1);
+        int rc = write(fd, buffer.data(), buffer.length());
+        if (rc > 0)
+            buffer.erase(0, rc);
+        }
     virtual void read_timeout(int fd)
-	{
-	cerr << "fd " << fd << " had a read timeout." << endl;
-	}
+        {
+        cerr << "fd " << fd << " had a read timeout." << endl;
+        }
     virtual void write_timeout(int fd)
-	{
-	cerr << "fd " << fd << " had a write timeout." << endl;
-	}
+        {
+        cerr << "fd " << fd << " had a write timeout." << endl;
+        }
     virtual void error_condition(int fd)
-	{
-	cerr << "fd " << fd << " had an error condition." << endl;
-	}
+        {
+        cerr << "fd " << fd << " had an error condition." << endl;
+        }
     virtual void pollhup(int fd)
-	{
-	cerr << "fd " << fd << " has hung up." << endl;
-	}
+        {
+        cerr << "fd " << fd << " has hung up." << endl;
+        }
     char tmp[1024];
     string buffer;
     scheduler& mysched;
@@ -83,10 +83,10 @@ try
     my_handler.prop.write_timeout = 3;
     sched.register_handler(0, my_handler, my_handler.prop);
     while (!sched.empty())
-	{
-	sched.schedule();
-	//sched.dump(cerr);
-	}
+        {
+        sched.schedule();
+        //sched.dump(cerr);
+        }
 
     // done
     return 0;
