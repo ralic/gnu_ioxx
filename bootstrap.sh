3eff6db3201c0d563bc2e63156dfa10f4d4e385b
Giuseppe Scrivano <gscrivano@gnu.org>
2010-04-24 03:01:51 +0200

    bootstrap.sh (gnulib_modules): Rewrite as a space separated list.
    This change fixes portability problems on systems where /bin/sh is not
    bash, i.e. on FreeBSD.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,11 @@ else
   gnulibtool=gnulib-tool
 fi
 
-gnulib_modules=( git-version-gen gitlog-to-changelog gnupload
-		 maintainer-makefile announce-gen )
+gnulib_modules="git-version-gen gitlog-to-changelog gnupload \
+                 maintainer-makefile announce-gen"
 
-$gnulibtool --m4-base build-aux --source-base build-aux --import "${gnulib_modules[@]}"
+$gnulibtool --m4-base build-aux --source-base build-aux \
+    --import "$gnulib_modules"
 
 build-aux/gitlog-to-changelog >ChangeLog
 
